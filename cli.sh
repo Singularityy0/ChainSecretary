c000fc5e0183ec13402864592d78ff9e7705a9ca
singu <patrickbatemanconfession@gmail.com>
2026-05-17 19:02:39 +0530

    Uncomment JavaScript execution line in cli.sh
    Uncomment the line to run the JavaScript script.

--- cli.sh
+++ cli.sh
@@ -2,6 +2,6 @@
 FIXTURE=$1
 
 # Uncomment the line for your chosen language:
-# node javascript/index.js "$FIXTURE"
+
 # python3 python/main.py "$FIXTURE"
 # cargo run --manifest-path rust/Cargo.toml -- "$FIXTURE"
